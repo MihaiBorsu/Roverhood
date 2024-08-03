ba0dd2d82b284a03e7f603ff99e63094a27dd318
mihai.borsu@gmail.com <mihai.borsu@gmail.com>
2024-08-03 18:42:56 +0300

    image from link

--- app/src/main/java/com/example/mystaticimageapp/MainActivity.java
+++ app/src/main/java/com/example/mystaticimageapp/MainActivity.java
@@ -1,12 +1,50 @@
 package com.example.mystaticimageapp;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.ImageView;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.Target;
+
+import android.util.Log;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String IMAGE_URL = "https://i.imgur.com/eoWyMMW.jpeg";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imageView = findViewById(R.id.imageView);
+
+        // Load the image from the URL using Glide
+        Glide.with(this)
+             .load(IMAGE_URL)
+             .apply(new RequestOptions().override(Target.SIZE_ORIGINAL))
+             .diskCacheStrategy(DiskCacheStrategy.ALL)
+             .listener(new RequestListener<Drawable>() {
+                 @Override
+                 public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                     Log.e("Glide", "Image load failed", e);
+                     return false; // important to return false so the error placeholder can be placed
+                 }
+
+                 @Override
+                 public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, com.bumptech.glide.load.DataSource dataSource, boolean isFirstResource) {
+                     Log.d("Glide", "Image loaded successfully");
+                     return false;
+                 }
+             })
+             .into(imageView);
     }
 }
